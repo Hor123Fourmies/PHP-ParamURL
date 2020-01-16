7f2ad9a0658c00b86a2e9021e2b49f42a2dd9a8b
Hor123 <hortensere@aol.com>
2020-01-16 14:03:34 +0100

    --

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+
+#PHP - Les paramètres d'URL
+
+##Exercice 1
+##Faire une page index.php.
+##Tester sur cette page que tous les paramètres de cette URL existent
+##et les afficher: index.php?nom=Nemare&prenom=Jean
+
+
+
+
+##Exercice 2 Faire une page index.php. Tester sur cette page que le paramètre age existe et si c'est le cas l'afficher sinon le signaler : index.php?nom=Nemare&prenom=Jean
+
+##Exercice 3 Faire une page index.php. Tester sur cette page que tous les paramètres de cette URL existent et les afficher: index.php?dateDebut=2/05/2016&dateFin=27/11/2016
+
+##Exercice 4 Faire une page index.php. Tester sur cette page que tous les paramètres de cette URL existent et les afficher: index.php?langage=PHP&serveur=LAMP
+
+##Exercice 5 Faire une page index.php. Tester sur cette page que tous les paramètres de cette URL existent et les afficher: index.php?semaine=12
+
+##Exercice 6 Faire une page index.php. Tester sur cette page que tous les paramètres de cette URL existent et les afficher: index.php?batiment=12&salle=101
+
+/*
+Note :
+
+-  En PHP, pour vérifier qu'une variable est définie, on peut utiliser la fonction native isset()
+( Cherchez isset sur php.net )
+- Pour verifier si une variable ou une expression est vide, on peut utiliser la fonction native empty()
+( cherchez empty sur php.net )
+*/
